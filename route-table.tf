f08d4bf1232228f76f70182943df2953682c65c3
Adnan <adnanshaikh11501@gmail.com>
2024-01-09 00:09:19 +0530

    Route Table

---
+++ route-table.tf
@@ -0,0 +1,14 @@
+# Creates Publice Route Table 
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-rt"
+  }
+}
