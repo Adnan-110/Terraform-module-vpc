1790c6dce388cab146b5dbc3939eccf4d60d8cd6
Adnan <adnanshaikh11501@gmail.com>
2024-01-08 23:48:28 +0530

    Subnet Availability Zone

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "public-subnet" {
-    count       = length(var.PUBLIC_SUBNET_CIDR)
-    vpc_id      = aws_vpc.main.id
-    cidr_block  = element(var.PUBLIC_SUBNET_CIDR, count.index)
+    count               = length(var.PUBLIC_SUBNET_CIDR)
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = element(var.PUBLIC_SUBNET_CIDR, count.index)
+    availability_zone   = element(var.AZ, count.index)
 
     tags = {
         Name = "roboshop-public-subnet-${var.ENV}-${count.index+1}"
@@ -9,9 +10,10 @@ resource "aws_subnet" "public-subnet" {
 }
 
 resource "aws_subnet" "private-subnet" {
-    count       = length(var.PRIVATE_SUBNET_CIDR)
-    vpc_id      = aws_vpc.main.id
-    cidr_block  = element(var.PRIVATE_SUBNET_CIDR, count.index)
+    count               = length(var.PRIVATE_SUBNET_CIDR)
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = element(var.PRIVATE_SUBNET_CIDR, count.index)
+    availability_zone   = element(var.AZ, count.index)
 
     tags = {
         Name = "roboshop-private-subnet-${var.ENV}-${count.index+1}"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,4 @@ variable "ENV" {}
 variable "VPC_CIDR" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
+variable "AZ" {}
