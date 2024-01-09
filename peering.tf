1a3b77dcfeb8c330362187769891db9996026dda
Adnan <adnanshaikh11501@gmail.com>
2024-01-09 18:12:56 +0530

    Peering

--- peering.tf
+++ peering.tf
@@ -3,4 +3,8 @@
 resource "aws_vpc_peering_connection" "peer" {
   vpc_id = var.DEFAULT_VPC_ID
   peer_vpc_id   = aws_vpc.main.id
+
+  tags = {
+    Name = "VPC Peering between default and roboshop"
+  }
 }
