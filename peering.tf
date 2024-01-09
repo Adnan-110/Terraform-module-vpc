84c1fad70f137821e57a4dc5dfe147a2deee9020
Adnan <adnanshaikh11501@gmail.com>
2024-01-09 19:51:52 +0530

    Peering connection auto accept

--- peering.tf
+++ peering.tf
@@ -1,8 +1,9 @@
 #  Enables Network Peering between 2 different networks
 
 resource "aws_vpc_peering_connection" "peer" {
-  vpc_id = var.DEFAULT_VPC_ID
+  vpc_id        = var.DEFAULT_VPC_ID
   peer_vpc_id   = aws_vpc.main.id
+  auto_accept   = true      # This works only if both the VPCs are from same AWS account.  
 
   tags = {
     Name = "VPC Peering between default and roboshop"
