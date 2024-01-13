f9a71cc6535a578c7d1d34f8cb3e6bf3226caa02
Adnan <adnanshaikh11501@gmail.com>
2024-01-13 17:27:29 +0530

    Terraform-databases backend,docdb,statefile share

---
+++ outputs.tf
@@ -0,0 +1,31 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "VPC_CIDR" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+  value = aws_subnet.private-subnet.*.cidr_block
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+  value = aws_subnet.public-subnet.*.cidr_block
+}
+
+output "PRIVATE_SUBNET_IDS" {
+  value = aws_subnet.private-subnet.*.id
+}
+
+output "PUBLIC_SUBNET_IDS" {
+    value = aws_subnet.public-subnet.*.id
+}
+
+output "DEFAULT_VPC_CIDR" {
+  value = var.DEFAULT_VPC_CIDR
+}
+
+output "DEFAULT_VPC_ID" {
+  value = var.DEFAULT_VPC_ID
+}
