8b75ecb7b3418a317fb7c1e4ca4a9844fd517dd3
Adnan <adnanshaikh11501@gmail.com>
2024-01-13 20:10:40 +0530

    Terraform-vpc outputs

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "VPC_ID" {
-  value = aws_vpc.main.id
+    value  = aws_vpc.main.id 
 }
 
 output "VPC_CIDR" {
-  value = aws_vpc.main.cidr_block
-}
-
-output "PRIVATE_SUBNET_CIDR" {
-  value = aws_subnet.private-subnet.*.cidr_block
-}
-
-output "PUBLIC_SUBNET_CIDR" {
-  value = aws_subnet.public-subnet.*.cidr_block
+    value  = aws_vpc.main.cidr_block
 }
 
 output "PRIVATE_SUBNET_IDS" {
-  value = aws_subnet.private-subnet.*.id
+    value  = aws_subnet.private_subnet.*.id
 }
 
 output "PUBLIC_SUBNET_IDS" {
-    value = aws_subnet.public-subnet.*.id
+    value  = aws_subnet.public_subnet.*.id
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+    value  = aws_subnet.private_subnet.*.cidr_block
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+    value  = aws_subnet.public_subnet.*.cidr_block
 }
 
 output "DEFAULT_VPC_CIDR" {
-  value = var.DEFAULT_VPC_CIDR
+    value   = var.DEFAULT_VPC_CIDR
 }
 
 output "DEFAULT_VPC_ID" {
-  value = var.DEFAULT_VPC_ID
+    value   = var.DEFAULT_VPC_ID
 }
