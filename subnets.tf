2efeafb8da09aeae99c870789b12ad7ab80a72f0
Adnan <adnanshaikh11501@gmail.com>
2024-02-10 19:54:50 +0530

    spaces removed

--- subnets.tf
+++ subnets.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "public-subnet" {
-    count               = length(var.PUBLIC_SUBNET_CIDR)
-    vpc_id              = aws_vpc.main.id
-    cidr_block          = element(var.PUBLIC_SUBNET_CIDR, count.index)
-    availability_zone   = element(var.AZ, count.index)
+    count                   = length(var.PUBLIC_SUBNET_CIDR)
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = element(var.PUBLIC_SUBNET_CIDR, count.index)
+    availability_zone       = element(var.AZ, count.index)
     map_public_ip_on_launch = true
 
     tags = {
